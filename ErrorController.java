25ede85b3743fbf665ab725b39f3a3d0ac5ae7be
alisleiman6 <ali.perfect06@gmail.com>
2025-02-26 14:59:32 +1100

    ErrorController.java

---
+++ ErrorController.java
@@ -0,0 +1,17 @@
+package controller;
+import au.edu.uts.ap.javafx.Controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import model.InputException;
+
+public class ErrorController extends Controller<InputException>{
+   
+    public final InputException getException() {
+        return model;
+    }
+    
+    @FXML 
+    private void handleOkay(ActionEvent event) {
+        stage.close();
+    }
+}
